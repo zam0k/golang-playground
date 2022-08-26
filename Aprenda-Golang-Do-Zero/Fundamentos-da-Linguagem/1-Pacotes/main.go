212fdd5a64a40f48fdf378410af1b06a53465115
Kelly Castelo <kawokkun@gmail.com>
2022-08-26 15:15:40 -0300

    pacotes externos

--- Aprenda-Golang-Do-Zero/Fundamentos-da-Linguagem/1-Pacotes/main.go
+++ Aprenda-Golang-Do-Zero/Fundamentos-da-Linguagem/1-Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/Aprenda-Golang-Do-Zero/Fundamentos-da-Linguagem/1-Pacotes/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main.")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("algo@gmail.com")
+	fmt.Println(erro)
 }
--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module modulo
 
 go 1.19
 
-
 //similar a um package.json
+
+require github.com/badoux/checkmail v1.2.1 // indirect
