1f2a8ac3be283e9d32175004da02e8e2d8b293d3
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 13:21:26 -0300

    operadores

---
+++ Aprenda-Golang-Do-Zero/Fundamentos-da-Linguagem/5-Operadores/operadores.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+	/*
+		## Aritmeticos (nada anormal)
+
+		soma +
+		subtração -
+		divisão /
+		multiplicacao *
+		resto %
+
+		Não da pra fazer operações com tipos diferentes,
+		mesmo se forem dois ints
+
+		## Atribuição
+
+		variavel2 := "Algo"
+
+		##Operadores relacionais (nada anormal)
+
+		>, >=, ==, <=, <, !=
+
+		##Operadores lógicos
+
+		AND - &&
+		OR - ||
+		NEGAÇÃO - !
+
+		só tem esses três nativamente no golang
+
+		##Unarios (nada anormal)
+
+		numero++
+		numero += 15
+		numero--
+		numero -= 15
+
+		numero *= 3
+		numero /= 10
+		numero %= 3
+
+		no golang não pode usar prefix
+
+		##Ternario
+
+		Não existe em golang, aqui é só if else msm para manter a simplicidade :)
+
+	*/
+}
