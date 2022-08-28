5b829be5761a60000044638b22b1a0c62c300556
Kelly Castelo <kawokkun@gmail.com>
2022-08-28 13:05:10 -0300

    switch

---
+++ Aprenda-Golang-Do-Zero/Fundamentos-da-Linguagem/12-switch/switch.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+//switch
+// não precisa utilizar a clausula break
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido."
+	}
+}
+
+//Condição avaliada dentro de cada case
+
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda"
+	case numero == 3:
+		return "Terça"
+	case numero == 4:
+		return "Quarta"
+	case numero == 5:
+		return "Quinta"
+	case numero == 6:
+		return "Sexta"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número inválido."
+	}
+}
+
+//fallthrough
+
+func diaDaSemana3(numero int) string {
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough
+	case numero == 2:
+		diaDaSemana = "Segunda"
+	case numero == 3:
+		diaDaSemana = "Terça"
+	case numero == 4:
+		diaDaSemana = "Quarta"
+	case numero == 5:
+		diaDaSemana = "Quinta"
+	case numero == 6:
+		diaDaSemana = "Sexta"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número inválido."
+	}
+
+	return diaDaSemana //fallthrough vai fazer retornar segunda
+}
+
+func main() {
+	fmt.Println(diaDaSemana(1))
+	fmt.Println(diaDaSemana2(2))
+	fmt.Println(diaDaSemana3(1))
+}
