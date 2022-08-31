653d74e82f528181faec5ac46a318d790158bf33
Kelly Castelo <kawokkun@gmail.com>
2022-08-31 14:50:42 -0300

    criando a funcionalidade busca por ip

--- Aprenda-Golang-Do-Zero/mini-aplicacao-de-comando/app/app.go
+++ Aprenda-Golang-Do-Zero/mini-aplicacao-de-comando/app/app.go
@@ -1,12 +1,47 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
+
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "zam0k.github.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	//net
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
