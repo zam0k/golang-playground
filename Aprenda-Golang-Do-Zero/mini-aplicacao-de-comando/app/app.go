e1ea860e6c4a46f31f54bda9e27df94f58597614
Kelly Castelo <kawokkun@gmail.com>
2022-08-31 14:24:51 -0300

    estruturando a aplicação de linha de comando

---
+++ Aprenda-Golang-Do-Zero/mini-aplicacao-de-comando/app/app.go
@@ -0,0 +1,12 @@
+package app
+
+import "github.com/urfave/cli"
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+
+	return app
+}
